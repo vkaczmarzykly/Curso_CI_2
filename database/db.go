5153c1ff9230110771cd241fe1c7de985c5a12d2
vkaczmarzykly <vkaczmarzykly@gmail.com>
2025-12-26 14:14:36 -0300

    Aula_2

--- database/db.go
+++ database/db.go
@@ -18,10 +18,10 @@ var (
 
 func ConectaComBancoDeDados() {
 	stringDeConexao := "host=" + os.Getenv("HOST") +
-		"user=" + os.Getenv("ROOT") +
-		"password=" + os.Getenv("PASSWORD") +
-		"dbname=" + os.Getenv("DBNAME") + "port=" + os.Getenv("PORT") +
-		"sslmode=disable"
+		" user=" + os.Getenv("ROOT") +
+		" password=" + os.Getenv("PASSWORD") +
+		" dbname=" + os.Getenv("DBNAME") + "port=" + os.Getenv("PORT") +
+		" sslmode=disable"
 	DB, err = gorm.Open(postgres.Open(stringDeConexao))
 	if err != nil {
 		log.Panic("Erro ao conectar com banco de dados")
